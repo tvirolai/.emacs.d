ee7c4abad94c19d297717c0dd9ee9ff81f724698
tvirolai <tuomo.virolainen@siili.com>
2025-07-29 13:59:26 +0300

    Tweaks

--- early-init.el
+++
@@ -1,6 +0,0 @@
-;; Temporarily increase GC threshold during startup
-(setq gc-cons-threshold most-positive-fixnum)
-
-;; Restore to normal value after startup (e.g. 50MB)
-(add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
